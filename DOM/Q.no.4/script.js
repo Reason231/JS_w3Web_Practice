f0daecb442aa8a7e233ac05cc66af78ef13e24d8
Reason231 <rijankhadgi100@gmail.com>
2024-09-12 16:27:06 +0545

    12th Sept

---
+++ DOM/Q.no.4/script.js
@@ -0,0 +1,8 @@
+const link=document.getElementById("w3r")
+function getAttributes(){
+    console.log(link.getAttribute("type"))
+    console.log(link.getAttribute("hreflang"))
+    console.log(link.getAttribute("rel"))
+    console.log(link.getAttribute("target"))
+    console.log(link.getAttribute("href"))
+}
