35b2da69ea547230ffd10a950379c750df926514
Reason231 <rijankhadgi100@gmail.com>
2024-10-17 10:03:20 +0545

    17th October

--- Array/Q.no.8.js
+++ Array/Q.no.8.js
@@ -5,3 +5,25 @@
 var arr1=[3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 arr1.sort()
 console.log(arr1)
+let frequency=0
+let mostFrequency=1
+let item;
+
+for (let i = 0; i < arr1.length; i++) {
+    
+    for (let j = 1; j <= arr1.length; j++) {
+        if(arr1[i] == arr1[j]){
+            frequency++
+        }
+
+        // If the new item frequency is greater than the previous item frequency then it updates the new frequency 
+        if (frequency>mostFrequency){
+            mostFrequency=frequency
+            item=arr1[i]
+        }
+    }
+    
+    // Reset the current item's frequency for the next iteration
+    frequency=0
+}
+console.log(`item (${item}) => ${mostFrequency} times repeated`)
